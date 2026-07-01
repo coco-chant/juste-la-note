3482ba3961c7a3f69fd9319e64a35c09048b864b
coco-chant <cambetcorentin@gmail.com>
2026-07-01 20:00:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
      (rapides, ils changent rarement).
    Quand on met l'app à jour, on incrémente le numéro de version ci-dessous. */
 
-const CACHE = "justenote-v8";
+const CACHE = "justenote-v9";
 
 // Fichiers qui composent l'app ("app shell").
 const ASSETS = [
